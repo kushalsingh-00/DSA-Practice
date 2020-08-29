988ed589201227d4f312aa91116328b54974ea39
kushalsingh-00 <kushalsinghrathore7@gmail.com>
2020-08-29 16:49:34 +0530

    Inverse Permutation

---
+++ GFG/Arrays/A3.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class A3 {
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        int t=s.nextInt();
+        for(int i=0;i<t;i++)
+        {
+            int n;
+            n=s.nextInt();
+            int a[]=new int[n];
+            for (int j = 1; j <= n; j++) {
+                a[s.nextInt()-1]=j;
+            }
+            for (int j : a) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+        s.close();
+    }
+}
